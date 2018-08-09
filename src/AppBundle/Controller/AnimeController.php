a0f51e1a4d61aaa5c83526e7bfe51cddd0c11b32
Ketsuen <kevin.git@lmdsio.fr>
2018-08-09 20:09:32 +0200

    first commit

---
+++ src/AppBundle/Controller/AnimeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 09/08/2018
+ * Time: 20:08
+ */
+
+namespace AppBundle\Controller;
+
+
+class AnimeController
+{
+
+}
---
+++ src/AppBundle/Entity/Anime.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 09/08/2018
+ * Time: 20:09
+ */
+
+namespace AppBundle\Entity;
+
+
+class Anime
+{
+
+}
