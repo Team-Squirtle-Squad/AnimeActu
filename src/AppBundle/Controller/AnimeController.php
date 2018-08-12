c4d54e35780c4ffee819c57ca0103d8fe92e6c59
Tomy-sama <tom_nguyen0@hotmail.fr>
2018-08-12 19:11:19 +0200

    vrai

--- src/AppBundle/Controller/AnimeController.php
+++ src/AppBundle/Controller/AnimeController.php
@@ -10,17 +10,20 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Genre;
+use AppBundle\Entity\Theme;
 use AppBundle\Entity\TypeAnime;
 use AppBundle\Form\GenreType;
+use AppBundle\Form\ThemeType;
 use AppBundle\Form\TypeAnimeType;
 use AppBundle\Services\GenreService;
+use AppBundle\Services\ThemeService;
 use AppBundle\Services\TypeAnimeService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class AnimeController extends Controller
 {
-    // Genre
+    // Genre //
     public function listeGenreAction(GenreService $genreService){
         return $this->render('@App/listeGenre.html.twig', array("genres" => $genreService->getAllGenre()));
     }
@@ -44,8 +47,9 @@ class AnimeController extends Controller
 
         return $this->render('@App/formulaireGenre.html.twig', array('form' => $form->createView()));
     }
+    // Fin Genre //
 
-    // Type d'anime
+    // Type d'anime //
     public function listeTypeAnimeAction(TypeAnimeService $typeAnimeService){
         return $this->render('@App/listeTypeAnime.html.twig', array("typesAnimes" => $typeAnimeService->getAllTypeAnime()));
     }
@@ -69,5 +73,29 @@ class AnimeController extends Controller
 
         return $this->render('@App/formulaireGenre.html.twig', array('form' => $form->createView()));
     }
+    // Fin Type d'anime //
+
+    // Theme //
+    public function addAndUpdateThemeAction(Request $request, ThemeService $themeService, $id = null)
+    {
+        if ($id === null) {
+            $theme = new Theme();
+        } else {
+            $theme = $themeService->getTheme($id);
+        }
+        $form = $this->createForm(ThemeType::class, $theme);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $themeService->save($theme);
+            $this->addFlash('info', 'L\'ajout de votre Thème : "' . $theme->getLibelleTheme() . '" à été validé');
+            return $this->redirectToRoute('liste_theme');
+        }
+
+        return $this->render('@App/formulaireGenre.html.twig', array('form' => $form->createView()));
+    }
+    // Fin Thème //
+
+
 
 }
