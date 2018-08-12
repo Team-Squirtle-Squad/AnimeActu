0a83f2941c451eeaf08bcd11470ce71eb0f79e45
Ketsuen <kevin.git@lmdsio.fr>
2018-08-12 16:41:19 +0200

    first commit

--- src/AppBundle/Controller/AnimeController.php
+++ src/AppBundle/Controller/AnimeController.php
@@ -9,7 +9,37 @@
 namespace AppBundle\Controller;
 
 
-class AnimeController
+use AppBundle\Entity\Genre;
+use AppBundle\Form\GenreType;
+use AppBundle\Services\GenreService;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AnimeController extends Controller
 {
+    public function listeGenreAction(GenreService $genreService){
+        return $this->render('@App/listeGenre.html.twig', array("genres" => $genreService->getAllGenre()));
+    }
+
+    public function addAndUpdateGenreAction(Request $request, GenreService $genreService, $id = null)
+    {
+        if ($id === null) {
+            $genre = new Genre();
+        } else {
+            $genre = $genreService->getGenre($id);
+        }
+        $form = $this->createForm(GenreType::class, $genre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $genreService->save($genre);
+            $this->addFlash('info', 'L\'ajout de votre genre : "' . $genre->getLibelleGenre() . '" à été validé');
+            return $this->redirectToRoute('liste_genre');
+        }
+
+        return $this->render('@App/formulaireGenre.html.twig', array('form' => $form->createView()));
+    }
+
 
 }
--- src/AppBundle/Entity/Anime.php
+++ src/AppBundle/Entity/Anime.php
@@ -9,7 +9,289 @@
 namespace AppBundle\Entity;
 
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="anime")
+ */
 class Anime
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $synopsis;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $couverture;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $video;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Genre")
+     */
+    private $genres;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Theme")
+     */
+    private $themes;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\TypeAnime")
+     * @ORM\Column(nullable=false)
+     */
+    private $typeAnime;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Anime
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set synopsis
+     *
+     * @param string $synopsis
+     *
+     * @return Anime
+     */
+    public function setSynopsis($synopsis)
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    /**
+     * Get synopsis
+     *
+     * @return string
+     */
+    public function getSynopsis()
+    {
+        return $this->synopsis;
+    }
+
+    /**
+     * Set couverture
+     *
+     * @param string $couverture
+     *
+     * @return Anime
+     */
+    public function setCouverture($couverture)
+    {
+        $this->couverture = $couverture;
+
+        return $this;
+    }
+
+    /**
+     * Get couverture
+     *
+     * @return string
+     */
+    public function getCouverture()
+    {
+        return $this->couverture;
+    }
+
+    /**
+     * Set video
+     *
+     * @param string $video
+     *
+     * @return Anime
+     */
+    public function setVideo($video)
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    /**
+     * Get video
+     *
+     * @return string
+     */
+    public function getVideo()
+    {
+        return $this->video;
+    }
+
+    /**
+     * Set genres
+     *
+     * @param string $genres
+     *
+     * @return Anime
+     */
+    public function setGenres($genres)
+    {
+        $this->genres = $genres;
+
+        return $this;
+    }
+
+    /**
+     * Get genres
+     *
+     * @return string
+     */
+    public function getGenres()
+    {
+        return $this->genres;
+    }
+
+    /**
+     * Set themes
+     *
+     * @param string $themes
+     *
+     * @return Anime
+     */
+    public function setThemes($themes)
+    {
+        $this->themes = $themes;
+
+        return $this;
+    }
+
+    /**
+     * Get themes
+     *
+     * @return string
+     */
+    public function getThemes()
+    {
+        return $this->themes;
+    }
+
+    /**
+     * Set typeAnime
+     *
+     * @param string $typeAnime
+     *
+     * @return Anime
+     */
+    public function setTypeAnime($typeAnime)
+    {
+        $this->typeAnime = $typeAnime;
+
+        return $this;
+    }
+
+    /**
+     * Get typeAnime
+     *
+     * @return string
+     */
+    public function getTypeAnime()
+    {
+        return $this->typeAnime;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->genres = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->themes = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add genre
+     *
+     * @param \AppBundle\Entity\Genre $genre
+     *
+     * @return Anime
+     */
+    public function addGenre(\AppBundle\Entity\Genre $genre)
+    {
+        $this->genres[] = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Remove genre
+     *
+     * @param \AppBundle\Entity\Genre $genre
+     */
+    public function removeGenre(\AppBundle\Entity\Genre $genre)
+    {
+        $this->genres->removeElement($genre);
+    }
+
+    /**
+     * Add theme
+     *
+     * @param \AppBundle\Entity\Theme $theme
+     *
+     * @return Anime
+     */
+    public function addTheme(\AppBundle\Entity\Theme $theme)
+    {
+        $this->themes[] = $theme;
+
+        return $this;
+    }
+
+    /**
+     * Remove theme
+     *
+     * @param \AppBundle\Entity\Theme $theme
+     */
+    public function removeTheme(\AppBundle\Entity\Theme $theme)
+    {
+        $this->themes->removeElement($theme);
+    }
 }
