c91c1149e0a5dc49e3f666232bbeb38a54e62388
Ketsuen <kevin.git@lmdsio.fr>
2018-08-15 21:12:28 +0200

    ajout des users & roles

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 15/08/2018
+ * Time: 21:04
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="integer", options={"unsigned":true, "default":0})
+     */
+    protected $experience;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $avatar;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    /**
+     * Set experience
+     *
+     * @param integer $experience
+     *
+     * @return User
+     */
+    public function setExperience($experience)
+    {
+        $this->experience = $experience;
+
+        return $this;
+    }
+
+    /**
+     * Get experience
+     *
+     * @return integer
+     */
+    public function getExperience()
+    {
+        return $this->experience;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return User
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+}
